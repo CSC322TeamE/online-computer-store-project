5094a65a41beeb6a42c766dea3ef42452e5f629e
spimta <spimtalin@gmail.com>
2021-04-23 06:08:07 -0400

    login and register check

--- onlineComputerStoreProject/onlineComputerStore/models.py
+++ onlineComputerStoreProject/onlineComputerStore/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 
-class LoginVerification(models.Model):
+class Users(models.Model):
     username = models.CharField(max_length=20)
     email = models.CharField(max_length=20)
     password = models.CharField(max_length=20)
--- onlineComputerStoreProject/onlineComputerStore/views.py
+++ onlineComputerStoreProject/onlineComputerStore/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
-from django.http import HttpResponseRedirect
+from django.shortcuts import render, redirect
+from onlineComputerStore.models import Users
 from django.contrib import messages
-from django.shortcuts import render_to_response
+
 
 # Create your views here.
 
@@ -11,19 +11,41 @@ def index(request):
 
 
 def login(request):
+    if request.POST:
+        username = request.POST['username']
+        password = request.POST['password']
+        if Users.objects.all().filter(username=username).filter(password=password).exists():
+            return redirect('/regUser/', {'user': username})
+
     return render(request, 'login.html')
 
 
 def register(request):
-    if request.POST:
-        return render(request, 'register.html', context={'success': 'true'},)
+    if 'email' in request.POST and 'username' in request.POST and 'password' in request.POST:
+        if Users.objects.all().filter(username=request.POST['username']).exists():
+            messages.info(request, 'Please use another username')
+            return render(request, 'register.html')
+
+        if Users.objects.all().filter(email=request.POST['email']).exists():
+            messages.info(request, 'Please use another email')
+            return render(request, 'register.html')
+
+        Users.objects.create(username=request.POST['username'],
+                             email=request.POST['email'],
+                             password=request.POST['password'])
+
+        return redirect('/login/')
+
     return render(request, 'register.html')
+
 
 def clerk(request):
     return render(request, 'clerk.html')
 
+
 def manager(request):
     return render(request, 'manager.html')
+
 
 def regUser(request):
     return render(request, 'regUser.html')
