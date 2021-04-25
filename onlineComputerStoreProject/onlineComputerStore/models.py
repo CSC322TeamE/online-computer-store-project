f396ab737ead3b8a2eec111c523623b64d296873
Yi Yang Wu <yywu2269015503@gmail.com>
2021-04-24 20:01:45 -0400

    test
    test

--- onlineComputerStoreProject/onlineComputerStore/models.py
+++ onlineComputerStoreProject/onlineComputerStore/models.py
@@ -7,6 +7,7 @@ class Users(models.Model):
     username = models.CharField(max_length=20)
     email = models.CharField(max_length=20)
     password = models.CharField(max_length=20)
+    creditCard = models.IntegerField()
 
 
 class Items(models.Model):
