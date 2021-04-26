f4bd68faa1c5578f8e00d2a18cdadf6a97f47bc2
Yi Yang Wu <yywu2269015503@gmail.com>
2021-04-26 04:12:12 -0400

    fix the bug
    I find out the I still are using the old version of code in models.py.

--- onlineComputerStoreProject/onlineComputerStore/models.py
+++ onlineComputerStoreProject/onlineComputerStore/models.py
@@ -1,16 +1,33 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Customer(User):
+    balance = models.FloatField(default=0.0)
+
+    class Meta:
+        permissions = [
+            ("add_balance", "can add balance"),
+        ]
 
 
-class Users(models.Model):
-    username = models.CharField(max_length=20)
-    email = models.CharField(max_length=20)
-    password = models.CharField(max_length=20)
-    creditCard = models.IntegerField()
+class Clerk(User):
+    pass
 
 
-class Items(models.Model):
+class Manager(Clerk):
+    pass
+
+
+class Company(User):
+    pass
+
+
+class DeliveryCompany(User):
+    pass
+
+
+class Item(models.Model):
     name = models.CharField(max_length=20)
     price = models.FloatField()
     quantity = models.IntegerField(default=0)
@@ -20,5 +37,27 @@ class Items(models.Model):
     quantity_sold = models.IntegerField(default=0)
 
 
+class CPU(Item):
+    category = models.CharField(max_length=10) #
+    core_name = models.CharField(max_length=10) # either intel or AMD
+    num_cores = models.IntegerField(default=0)
+    frequency = models.FloatField(default=0.0)
 
 
+class GPU(Item):
+    category = models.CharField(max_length=10)  # desktop or workstation
+    chipset = models.CharField(max_length=10)  # either nvidia or AMD
+    num_cuda_cores = models.IntegerField(default=0)
+    core_clock = models.FloatField(default=0.0)
+
+
+class Memory(Item):
+    capacity = models.FloatField(default=0.0)
+
+
+class Computer(Item):
+    category = models.CharField(max_length=20)  # business, computing, gaming ...
+    os = models.CharField(max_length=10) # operating system
+    cpu_id = models.IntegerField()  # cpu
+    gpu_id = models.IntegerField()  # gpu
+    memory_id = models.IntegerField()  # memory
