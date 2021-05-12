eebfb21bc41492f39a7a666f6031ff8931debac0
Lsx961029 <shuoxinliu961029@gmail.com>
2021-05-11 23:17:48 -0400

    forced push

--- onlineComputerStoreProject/onlineComputerStore/tests.py
+++ onlineComputerStoreProject/onlineComputerStore/tests.py
@@ -37,13 +37,37 @@ def add_user():
         group = Group.objects.get(name='clerks')
         user.groups.add(group)
 
+    if not Clerk.objects.filter(username="ShuoXin Liu").exists():
+        user = Clerk.objects.create_user(username="ShuoXin Liu", password="ShuoXin Liu")
+        group = Group.objects.get(name='clerks')
+        user.groups.add(group)
+
+    if not Clerk.objects.filter(username="Yi Lin").exists():
+        user = Clerk.objects.create_user(username="Yi Lin", password="Yi Lin")
+        group = Group.objects.get(name='clerks')
+        user.groups.add(group)
+
+    if not Clerk.objects.filter(username="Ling Fang").exists():
+        user = Clerk.objects.create_user(username="Ling Fang", password="Ling Fang")
+        group = Group.objects.get(name='clerks')
+        user.groups.add(group)
+
+    if not Clerk.objects.filter(username="Yi Yang Wu").exists():
+        user = Clerk.objects.create_user(username="Yi Yang Wu", password="Yi Yang Wu")
+        group = Group.objects.get(name='clerks')
+        user.groups.add(group)
+
+    if not Clerk.objects.filter(username="Alice Chen").exists():
+        user = Clerk.objects.create_user(username="Alice Chen", password="Alice Chen")
+        group = Group.objects.get(name='clerks')
+        user.groups.add(group)
+
     if not Manager.objects.filter(username="manager").exists():
         user = Manager.objects.create_user(username="manager", password="manager")
         group = Group.objects.get(name='managers')
         user.groups.add(group)
 
-
-#Dlivery Company
+    # Dlivery Company
     if not DeliveryCompany.objects.filter(username="UPS").exists():
         user = DeliveryCompany.objects.create_user(username="UPS", password="UPS")
         group = Group.objects.get(name='deliverycompanies')
@@ -54,9 +78,66 @@ def add_user():
         group = Group.objects.get(name='deliverycompanies')
         user.groups.add(group)
 
+    if not DeliveryCompany.objects.filter(username="DHL").exists():
+        user = DeliveryCompany.objects.create_user(username="DHL", password="DHL")
+        group = Group.objects.get(name='deliverycompanies')
+        user.groups.add(group)
 
+    if not DeliveryCompany.objects.filter(username="USPS").exists():
+        user = DeliveryCompany.objects.create_user(username="USPS", password="USPS")
+        group = Group.objects.get(name='deliverycompanies')
+        user.groups.add(group)
 
+    if not DeliveryCompany.objects.filter(username="Purolator").exists():
+        user = DeliveryCompany.objects.create_user(username="Purolator", password="Purolator")
+        group = Group.objects.get(name='deliverycompanies')
+        user.groups.add(group)
+
+    if not DeliveryCompany.objects.filter(username="OnTrac Inc.").exists():
+        user = DeliveryCompany.objects.create_user(username="OnTrac Inc.", password="OnTrac Inc.")
+        group = Group.objects.get(name='deliverycompanies')
+        user.groups.add(group)
     if not Company.objects.filter(username="INTEL").exists():
         user = Company.objects.create_user(username="INTEL", password="INTEL")
+        group = Group.objects.get(name='companies')
+        user.groups.add(group)
+
+    if not Company.objects.filter(username="AMD").exists():
+        user = Company.objects.create_user(username="AMD", password="AMD")
+        group = Group.objects.get(name='companies')
+        user.groups.add(group)
+
+    if not Company.objects.filter(username="HP").exists():
+        user = Company.objects.create_user(username="HP", password="HP")
+        group = Group.objects.get(name='companies')
+        user.groups.add(group)
+
+    if not Company.objects.filter(username="APPLE").exists():
+        user = Company.objects.create_user(username="APPLE", password="APPLE")
+        group = Group.objects.get(name='companies')
+        user.groups.add(group)
+
+    if not Company.objects.filter(username="DELL").exists():
+        user = Company.objects.create_user(username="DELL", password="DELL")
+        group = Group.objects.get(name='companies')
+        user.groups.add(group)
+
+    if not Company.objects.filter(username="LENOVO").exists():
+        user = Company.objects.create_user(username="LENOVO", password="LENOVO")
+        group = Group.objects.get(name='companies')
+        user.groups.add(group)
+
+    if not Company.objects.filter(username="ACER").exists():
+        user = Company.objects.create_user(username="ACER", password="ACER")
+        group = Group.objects.get(name='companies')
+        user.groups.add(group)
+
+    if not Company.objects.filter(username="MICROSOFT").exists():
+        user = Company.objects.create_user(username="MICROSOFT", password="MICROSOFT")
+        group = Group.objects.get(name='companies')
+        user.groups.add(group)
+
+    if not Company.objects.filter(username="SAMSUNG").exists():
+        user = Company.objects.create_user(username="SAMSUNG", password="SAMSUNG")
         group = Group.objects.get(name='companies')
         user.groups.add(group)
