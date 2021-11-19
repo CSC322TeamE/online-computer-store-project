febd6bb001e9d3df0bc7d90d4c5e18b10843878d
Shuoxin Liu <80080566+Lsx961029@users.noreply.github.com>
2021-11-19 17:42:50 -0500

    remove sensitive info

--- onlineComputerStoreProject/onlineComputerStoreProject/settings.py
+++ onlineComputerStoreProject/onlineComputerStoreProject/settings.py
@@ -129,9 +129,9 @@ STATIC_DIRS = [
 # send email
 EMAIL_HOST = 'smtp.gmail.com'
 
-EMAIL_HOST_USER = 'onlineComputerStoreGroup@gmail.com'
+EMAIL_HOST_USER = ''
 
-EMAIL_HOST_PASSWORD = '12110411'
+EMAIL_HOST_PASSWORD = ''
 
 EMAIL_USE_TLS = True
 
